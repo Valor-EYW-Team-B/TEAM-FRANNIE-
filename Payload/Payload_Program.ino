f5dd572d0193cea07e84c657f2cc2f4f49fa4c7b
DraketheOtaku <jessicaousley@gmail.com>
2017-05-15 08:41:33 -0700

    Add files via upload

--- Payload/Payload_Program.ino
+++ Payload/Payload_Program.ino
@@ -2,7 +2,7 @@
 #include <Servo.h>
 #include <Wire.h>
 
-EYW::Camera Sebastian;
+EYW::Camera Sebastian; //because I can
 
 int butt=2;
 int led=4;
@@ -11,15 +11,14 @@ int speak=5;
 void setup() {
 Sebastian.begin(butt,led,speak);
 Sebastian.calibrate();
-Sebastian.alarm(1,267,500);
-Sebastian.beginTimer(3000);
-
+Sebastian.alarm(1,800,500); //high frequency to make it noticeable
+Sebastian.beginTimer(3000); //allows time for descent
 }
 
 void loop() {
 if (Sebastian.timerExpired()==true||Sebastian.buttonPressed()==true)
 {
-  Sebastian.getPicture();
-  Sebastian.beginTimer(5000);
+  Sebastian.getPicture(1,1); //take maximum amount of pictures within the general time frame
+  Sebastian.beginTimer(500); //allow a small pause between takes
 }
 }
