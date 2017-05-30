5df6d58e99355fe1c56bbee52441ef0794fc847b
PeacefulYoda <grantgwalker@live.com>
2017-05-30 10:33:25 -0700

    Moved bool

--- Everything/Everything.ino
+++ Everything/Everything.ino
@@ -1,27 +1,28 @@
-// test from Jerry
 #include <EYW.h>
-#include <Servo.h>
 #include <Wire.h>
+#include <Servo.h>
+
+
 
 EYW::Altimeter myaltimeter;
 EYW::RangeFinder proximity;
 int distance = 0;
 EYW::Camera Sebastian;
 float current_height = 0;
-
+bool altimeterheight= false;
 
 void setup() {
 
 Serial.begin(9600);
 proximity.begin();
-proximity.alarm();
+proximity.alarm(6, 500, 1000);
 proximity.calibrate(10);
 
 
 
 Sebastian.begin();
 Sebastian.calibrate();
-Sebastian.alarm(1,800,500);
+Sebastian.alarm(1,800,1000);
 Sebastian.beginTimer(1000);
 
 
@@ -29,7 +30,9 @@ Sebastian.beginTimer(1000);
 Serial.begin(9600);
 myaltimeter.begin();
 myaltimeter.calibrate(100);
-myaltimeter.alarm();
+myaltimeter.alarm(3, 200, 2000);
+
+
 
 }
 
@@ -37,32 +40,38 @@ myaltimeter.alarm();
 void loop() {
   current_height = myaltimeter.getHeightAvg (30);
   
-  if (Sebastian.timerExpired()==true && current_height >= 3)
-  {
-    Sebastian.getPicture();
-    Sebastian.beginTimer(1000);
-  }
-
-    distance = proximity.getDistance();
-    Serial.print("Current Distance:");
-    Serial.println(distance);
-
-
-
-if (distance <200) {
-  proximity.alarm(6 ,2000,100);
-  }
-
-
-
-current_height = myaltimeter.getHeightAvg(20);
 Serial.print("Current Height:");
 Serial.println(current_height);
 
 
 
-if (current_height>3)
-  {
-    myaltimeter.alarm(6,2000,500);
-  }
+if (current_height > 3) {
+  myaltimeter.alarm(2, 1500, 4000);
+}
+
+
+
+if (current_height >= 40 && current_height <= 50) {
+  altimeterheight= true;
+}
+
+
+if (current_height >= 30 && altimeterheight==true && current_height <= 40) {
+  Sebastian.getPicture();
+  Sebastian.beginTimer(3000);
+}
+
+
+if (current_height < 2) {
+  proximity.getDistance();
+  Serial.print("Current Distance:");
+  Serial.println(distance);
+}
+
+if (distance < 500){
+  proximity.alarm(10, 100, 500);
+  delay(300);
+}
+
+
 }
