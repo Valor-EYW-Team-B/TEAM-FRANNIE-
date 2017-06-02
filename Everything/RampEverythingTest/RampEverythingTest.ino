705a6ba7a3a0f6e4486406a8ad2880ebc79423a9
frothysaucey <jallen4th@gmail.com>
2017-06-02 13:08:47 -0700

    important changes for the ramp: changes numbers

--- Everything/RampEverythingTest/RampEverythingTest.ino
+++ Everything/RampEverythingTest/RampEverythingTest.ino
@@ -1,24 +1,23 @@
 #include <EYW.h>
-#include <Wire.h>
-#include <Servo.h>
 
 
 
 EYW::Altimeter myaltimeter;
 EYW::RangeFinder myRangeFinder;
-int distance = 0;
 EYW::Camera PServo;
+
 float current_height = 0;
 bool altimeterheight= false;
-
+int distance = 0;
 
 void setup() {
 
 Serial.begin(9600);
 
+myaltimeter.alarm(3, 700, 150);
 myaltimeter.begin();
 myaltimeter.calibrate(200);
-myaltimeter.alarm(3, 200, 2000);
+myaltimeter.alarm(1, 200, 2000);
 
 
 myRangeFinder.begin();
@@ -41,31 +40,31 @@ Serial.println(current_height);
 
 
 
-if (current_height > 0.5) {
+if (current_height > 1) {
   myaltimeter.alarm(2, 1500, 4000);
 }
 
 
 
-if (current_height >= 0.2 && current_height <= 3) {
+if (current_height >= 1 && current_height <= 3) {
   altimeterheight= true;
 }
 
 
-if (current_height >= 0.5 && altimeterheight==true && current_height <= 2.5) {
+if (current_height > 1 && altimeterheight==true && current_height > 2.5) {
   PServo.getPicture();
   PServo.beginTimer(3000);
 }
 
 
 if (current_height < 1) {
-  myRangeFinder.getDistance();
+  distance = myRangeFinder.getDistance();
   Serial.print("Current Distance:");
   Serial.println(distance);
 }
 
-if (distance < 500){
-  myRangeFinder.alarm(10, 100, 500);
+if (distance < 20){
+  myRangeFinder.alarm(3, 300, 200);
  
 }
 
